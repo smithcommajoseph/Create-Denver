a29f697446fec65c399907518c60a4e2e69cad70
Joseph Smith <technicolorenvy@gmail.com>
2012-05-06 11:41:34 -0600

    adding outlets, minor linting

--- Max/code/levelcontroller.js
+++ Max/code/levelcontroller.js
@@ -1,20 +1,20 @@
 // inlets and outlets
 inlets = 1;
-outlets = 2;
+outlets = 4;
 
 //Vars
 var currentLevel = 0,
 	lastLevel = 0,
 	level_type = "level";
 
-//Constants	
-	LEVEL_OVERLAY_NAME 	= "_overlay_",
+//Constants
+	LEVEL_OVERLAY_NAME	= "_overlay_",
 	LEVEL_BG_NAME		= "_bg_",
 	LEVEL_TRANS_NAME	= "_trans_",
 	JPG					= "jpg",
 	PNG					= "png",
 	MOV					= "mov",
-	PATH					= "/Users/joseph/Projects/create_denver/Max/media/";
+	PATH				= "/Users/joseph/Projects/create_denver/Max/media/";
 
 function bang(){
 	var ob = getResolvedOb();
@@ -26,8 +26,8 @@ function getResolvedOb(){
 	var ob = {};
 		
 	if(level_type != "level"){
-		ob.bg 		= PATH+"level"+LEVEL_TRANS_NAME+currentLevel+"."+MOV;
-		ob.overlay 	= PATH+"clear."+PNG;
+		ob.bg		= PATH+"level"+LEVEL_TRANS_NAME+currentLevel+"."+MOV;
+		ob.overlay	= PATH+"clear."+PNG;
 	} else {
 		ob.bg		= PATH+"level"+LEVEL_BG_NAME+currentLevel+"."+JPG;
 		ob.overlay	= PATH+"level"+LEVEL_OVERLAY_NAME+currentLevel+"."+PNG;
